4d4b230e10f9e209b8569947660cb1c52d304f8c
Booze33 <tebetisloh@gmail.com>
2024-08-09 11:20:26 +0200

    Inventory

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -8,6 +8,13 @@ export interface Product {
   stockQuantity: number;
 }
 
+export interface NewProduct {
+  name: string;
+  price: number;
+  rating?: number;
+  stockQuantity: number;
+}
+
 export interface SalesSummary {
   salesSummaryId: string;
   totalValue: number;
@@ -51,8 +58,23 @@ export const api = createApi({
     getDashboardMetrics: build.query<DashboardMetrics, void>({
       query: () => '/dashboard',
       providesTags: ["DashboardMetrics"]
+    }),
+    getProducts: build.query<Product[], string | void>({
+      query: (search) => ({
+        url: '/products',
+        params: search ? { search } : {}
+      }),
+      providesTags: ["Products"],
+    }),
+    createProduct: build.mutation<Product, NewProduct>({
+      query: (newProduct) => ({
+        url: '/products',
+        method: "POST",
+        body: newProduct
+      }),
+      invalidatesTags: ["Products"],
     })
   }),
 });
 
-export const { useGetDashboardMetricsQuery } = api;
+export const { useGetDashboardMetricsQuery, useGetProductsQuery, useCreateProductMutation } = api;
